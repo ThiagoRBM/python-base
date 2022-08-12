0b55dffed20ce46723d2c9a55f3a0a56abb26717
thiagorbm <thiagorbm@gmail.com>
2022-08-12 16:23:43 -0300

    tabuada v.3

--- tabuada.py
+++ tabuada.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+__version__ = "0.1.1"
+__author__ = "Thiago"
+
 seq=range(1,11)
 
 for i in seq:
-	print(f"{'-'*4} Tabuada do {i} {'-'*4}\n")
+	#print(f"{'-'*4} Tabuada do {i} {'-'*4}\n")
+	print("{:-^25}".format(f"Tabuada do {i}"))
 	for x in seq:
 		print("{:^25}".format(f"{i:03d} x {x:03d} = {i*x:03d}"))
 	print("#" * 25 + "\n")
