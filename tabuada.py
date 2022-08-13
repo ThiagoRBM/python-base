37a5ba7240f4223bd53ab470bde10a11dd55bef8
thiagorbm <thiagorbm@gmail.com>
2022-08-13 15:43:48 -0300

    tabuada

--- tabuada.py
+++ tabuada.py
@@ -6,9 +6,9 @@ __author__ = "Thiago"
 seq=range(1,11)
 
 for i in seq:
-	#print("{:-^25}".format(f"Tabuada do {i}"))
-	print("{:-^25}".format(f"Tabuada do \U00002460"))
-	print(f"\U00002460")
+	print("{:-^25}".format(f"Tabuada do {i}"))
+	#print("{:-^25}".format(f"Tabuada do \U00002460"))
+	#print(f"\U00002460")
 	for x in seq:
 		print("{:^25}".format(f"{i:03d} x {x:03d} = {i*x:03d}"))
 	print("#" * 25 + "\n")
