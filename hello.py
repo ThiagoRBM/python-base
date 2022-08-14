7f4f5baaba790b82fd3c836e1a97920721b3203a
thiagorbm <thiagorbm@gmail.com>
2022-08-14 15:17:04 -0300

    commit um

---
+++ hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+###################################################
+# ALTERE daqui usando dicionários
+###################################################
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!" 
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
+
+print(msg)
