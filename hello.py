e2d2d8cecd84110cc926a5f7036f82e2489fb72f
thiagorbm <thiagorbm@gmail.com>
2022-08-15 10:56:30 -0300

    commit 3, CLI

--- hello.py
+++ hello.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+#print(f"{sys.argv=}")
+#print(f"{sys.argv[1]=}")
+
+args= {"lang" : None,
+        "count" : None}
+
+for arg in sys.argv[1:]:
+    key, value= str(arg).split(sep="=")
+    key= key.lstrip("-").strip()
+    value= value.strip()
+    if key not in args.keys():
+            print(f"Argumento: `{key.lstrip('-')}` não reconhecido")
+            sys.exit()
+    args[key]= value
+    #print(arg)
+#print(args)
+    
+if args["lang"] is None:
+    current_language = os.getenv("LANG")
+    if current_language is None:
+        current_language= input("Choose your language: ")
+else:
+    current_language= args["lang"]
+
+#print(current_language)
+#print(args["--count"])
+
+if args["count"] == None:
+    #print("aqui")
+    count = 1
+else:
+    count= int(args["count"])
+#print(count)
 
 ###################################################
 # ALTERE daqui usando dicionários
@@ -14,4 +47,6 @@ msg= {"en_US" : "Hello, World!",
         "es_SP" : "Hola, Mundo!",
         "fr_FR" : "Bonjour, Monde!"}
 
-print(msg[current_language])
+mensagem= f"{msg[current_language]}\n"
+
+print(f"{mensagem * int(count)}")
