bf6193bf184058cec9f7b3750f0f9d84b19e8acb
thiagorbm <thiagorbm@gmail.com>
2022-08-16 13:00:18 -0300

    versao 2 calculadora com logs

--- infixcalc.py
+++ infixcalc.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 ## minha linha de raciocínio acho que foi bem diferente da sua. Não mexi no código (tanto que tem lá o "len(sys.argv[1:]) == 0" para ficar parecida com a sua. Apesar de diferente ela funcionou direitinho, mas eu não tinha pensado em tratar todos os posíveis erros (porém, prometo que pausava e resolvia eles assim que você mencionava, antes de ver o resultado, e funcionou; apanhei só com o método "isdigit" porque não lembrava que era usado em str).
+import os
 import sys
+from datetime import datetime
 
 operacao= None
 n= [None, None]
@@ -16,30 +18,37 @@ else:
     
 numeros= []
 for num in n:
-    if num.replace(".","").isdigit():
+    if not num.replace(".","").isdigit():
+        print("Digitar apenas numeros para realizar a operação")
+        print(f"Foi figitado: `{num}`")
+        sys.exit(1)
+    else:
         if "." in num:
             numeros.append(float(num))
         else:
             numeros.append(int(num))
-    else:
-        print("Digitar apenas numeros para realizar a operação")
-        print(f"Foi figitado: `{num}`")
-        sys.exit(1)
         
 if len([operacao]) + len(numeros) != 3:
     print("Número de argumentos inválidos")
     print("ex: sum 5 5 ")
     sys.exit(2)
 
-permitidas= ["sum","difference","product","quotient"]
+permitidas= ["sum","sub","mul","div"]
 if operacao not in permitidas:
     print(f"Operacoes permitidas: {permitidas}")
     print(f"Operacao requisitada: `{operacao}`")
     sys.exit(3)
     
 operacoes= {"sum": numeros[0] + numeros[1],
-            "difference": numeros[0] - numeros[1],
-            "product": numeros[0] * numeros[1],
-            "quotient": numeros[0] / numeros[1]}
+            "sub": numeros[0] - numeros[1],
+            "mul": numeros[0] * numeros[1],
+            "div": numeros[0] / numeros[1]}
+
+path= os.path.abspath(os.curdir)
+filepath= os.path.join(path,"calc_history.log")
+timestamp= datetime.now().isoformat()
+usr= os.getenv('USER', "annonymous")
+
+print(f"{timestamp} / {usr}: {operacao} of {numeros[0]} and {numeros[1]} equals {operacoes[operacao]}", file=open(filepath, "a"))
 
 print(operacoes[operacao])
