bfe4bbd467a0c684adc7589feb5a37468a207e5c
thiagorbm <thiagorbm@gmail.com>
2022-09-16 08:01:26 -0300

    aula orientacao a objetos

--- OO/pilares.py
+++ OO/pilares.py
@@ -132,3 +132,37 @@ conta.depositar(100)
 conta.sacar(10)
 print(conta.consultar())
 print(conta.cliente)
+
+## Encapsulamento com propriedades:
+
+
+class Conta:
+    _tipo_de_conta = "corrente"
+    __id_interno = 985645
+
+    def __init__(self, cliente):
+        self.cliente = cliente
+        self._saldo = 0
+
+    @property  # getter
+    def saldo(self):
+        if self._saldo < 0:
+            print("AVISO: Você está devendo")
+        return self._saldo
+
+    @saldo.setter
+    def saldo(self, value):
+        self._saldo += value
+
+    @saldo.deleter
+    def saldo(self):
+        self._saldo = 0
+
+
+conta = Conta(cliente="Bruno")
+conta.saldo = 100
+conta.saldo = -10
+print(conta.saldo)
+
+del conta.saldo
+print(conta.saldo)
